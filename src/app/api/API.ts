14947b9f716f5cfee9be675cf6f8210fd7f005df
jerry <jerrystarry@gmail.com>
2018-03-22 16:56:32 +0800

    修复bug

--- src/app/api/API.ts
+++ src/app/api/API.ts
@@ -1,7 +1,7 @@
 
 import { HttpHeaders } from '@angular/common/http';
 export class Api {
-    static url = { keyword: '/recipe/search?keyword=', item: '/recipe/class', classid: '/recipe/byclass?classid=' ,foodid:'/recipe/detail?id='};
+    static url = { keyword: 'http://jisusrecipe.market.alicloudapi.com/recipe/search?keyword=', item: 'http://jisusrecipe.market.alicloudapi.com/recipe/class', classid: 'http://jisusrecipe.market.alicloudapi.com/recipe/byclass?classid=' ,foodid:'http://jisusrecipe.market.alicloudapi.com/recipe/detail?id='};
     static httpHeaders = {
         headers: new HttpHeaders({ 'Authorization': 'APPCODE 8404fb2810f045818810cbd64cdfd3a3' })
     }
--- src/app/foodList/foodList.component.ts
+++ src/app/foodList/foodList.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, HostListener, AfterContentInit} from '@angular/core';
 
 import { FoodService } from '../service/food.service';
-import { ResponDish, Dishes } from '../eneity/ResponDish';
+import { ResponDish, Dishes, Result } from '../eneity/ResponDish';
 
 @Component({
   selector: 'app-food-list',
@@ -12,32 +12,33 @@ export class FoodListComponent implements OnInit, AfterContentInit {
   @Input() dishes: Dishes[];
   @Input('q') q: string;
   @Input('id') id: string;
-  start: number = 10;
+  start = 10;
   responDish: ResponDish;
-
+  result: Result;
   constructor(private foodService: FoodService) { }
 
   ngOnInit() { }
   ngAfterContentInit() {
-    
     this.onWindowScroll();
   }
-  @HostListener("window:scroll", ["$event"])
+  @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
-    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
-    let max = document.documentElement.scrollHeight;
-    if (pos == max) {
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    const max = document.documentElement.scrollHeight;
+    if (pos === max) {
       if (this.id) {
-        this.foodService.getFoodListByCid(this.id, this.start).subscribe(result => this.responDish = result);
-        this.dishes = this.dishes.concat(this.responDish.result.list);
+        this.foodService.getFoodListByCid(this.id, this.start)
+        .subscribe(result => { this.responDish = result; this.result = this.responDish.result; });
+        this.dishes = this.dishes.concat(this.result.list);
         this.start = this.start + 5;
       }
       if (this.q) {
-        this.foodService.getFoodListByKey(this.q, this.start).subscribe(result => this.responDish = result);
-        this.dishes = this.dishes.concat(this.responDish.result.list);
+        this.foodService.getFoodListByKey(this.q, this.start)
+        .subscribe(result => { this.responDish = result; this.result = this.responDish.result; });
+        this.dishes = this.dishes.concat(this.result.list);
         this.start = this.start + 5;
       }
-    };
+    }
   }
 }
 
