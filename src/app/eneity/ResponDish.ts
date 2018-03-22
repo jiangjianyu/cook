3733cb6d86a1572c17b4d640f8723fcad5375c66
jerry <jerrystarry@gmail.com>
2018-03-22 13:42:05 +0800

    add <ng-container>

--- src/app/eneity/ResponDish.ts
+++ src/app/eneity/ResponDish.ts
@@ -1,37 +1,37 @@
-export class ResponDish{
-    status:string;
-    msg:string;
-    result:Result;
+export class ResponDish {
+    status: string;
+    msg: string;
+    result: Result;
 }
-export class Result{
-    num:string;
-    list:Dishes[];
+export class Result {
+    num: string;
+    list: Dishes[];
 
 }
-export class Dishes{
-    id:string;
-    classid:string;
-    name:string;
-    peoplenum:string;
-    preparetime:string;
-    cookingtime:string;
-    content:string;
-    pic:string;
-    tag:string;
-    material:Material[];
+export class Dishes {
+    id: string;
+    classid: string;
+    name: string;
+    peoplenum: string;
+    preparetime: string;
+    cookingtime: string;
+    content: string;
+    pic: string;
+    tag: string;
+    material: Material[];
     process: Process[];
 }
-class Material{
-    mname:string;
+class Material {
+    mname: string;
     type: string;
     amount: string;
 }
-class Process{
-    pcontent:string;
+class Process {
+    pcontent: string;
     pic: string;
 }
-export class Food{
-    status :string;
+export class Food {
+    status: string;
     mmsg: string;
-    result:Dishes;
+    result: Dishes;
 }
