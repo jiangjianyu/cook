1b29d7d5380b25319049a55bed34b8e7c305bd67
jerry <jerrystarry@gmail.com>
2018-03-23 09:41:15 +0800

    update api

--- src/app/api/API.ts
+++ src/app/api/API.ts
@@ -1,9 +1,15 @@
 
 import { HttpHeaders } from '@angular/common/http';
 export class Api {
-    static url = { keyword: 'http://jisusrecipe.market.alicloudapi.com/recipe/search?keyword=', item: 'http://jisusrecipe.market.alicloudapi.com/recipe/class', classid: 'http://jisusrecipe.market.alicloudapi.com/recipe/byclass?classid=' ,foodid:'http://jisusrecipe.market.alicloudapi.com/recipe/detail?id='};
+    // http://jisusrecipe.market.alicloudapi.com/s
+    static url = {
+        keyword: 'recipe/search?keyword=',
+        item: 'recipe/class',
+        classid: 'recipe/byclass?classid=',
+        foodid: 'recipe/detail?id='
+    };
     static httpHeaders = {
         headers: new HttpHeaders({ 'Authorization': 'APPCODE 8404fb2810f045818810cbd64cdfd3a3' })
-    }
-    static page = '&num=10&start='; 
+    };
+    static page = '&num=10&start=';
 }
